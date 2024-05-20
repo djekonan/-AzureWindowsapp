a641511b3f49408e3a76d73141e887e62b471340
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-20 11:34:01 -0400

    Create winappsever.tf

---
+++ dev/winappsever.tf
@@ -0,0 +1,21 @@
+
+resource "azurerm_service_plan" "svregiswinserverappexample" {
+  for_each            = {for windowappserver in local.window_app_list : "${windowappserver.name}" => windowappserver}
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.myregiswindowappserverrg.name
+  location            = azurerm_resource_group.myregiswindowappserverrg.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
+}
+
+
+resource "azurerm_windows_web_app" "example" {
+  for_each            = { for app in local.window_app_list : app.name => app }
+
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.myregiswindowappserverrg.name
+  service_plan_id     = azurerm_service_plan.svregiswinserverappexample[each.key].id
+  location            = azurerm_service_plan.svregiswinserverappexample[each.key].location
+
+  site_config {}
+}
