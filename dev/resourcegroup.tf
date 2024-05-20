bd77bb01f06e8606efaf425d871e0adc1aa29e98
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-20 11:35:43 -0400

    Update resourcegroup.tf

--- dev/resourcegroup.tf
+++ dev/resourcegroup.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_resource_group" "myregiswindowappserverrg" {
+resource "azurerm_resource_group" "konanwindowappserverrg" {
   name     = "mylearningresourcegrouptest"
   location = "canadacentral"
 }
